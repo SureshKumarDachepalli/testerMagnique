2da190c55f464448380794b6130e77384f18d588
SureshKumarDachepalli <sureshkumar.d467@gmail.com>
2024-06-13 21:53:27 +0530

    commit msg project

---
+++ src/main/java/pkg2/CCC.java
@@ -0,0 +1,8 @@
+package pkg2;
+
+public class CCC {
+	int x;
+	int y;
+	int z;
+
+}
---
+++ src/test/java/pkg1/AAA.java
@@ -0,0 +1,13 @@
+package pkg1;
+
+public class AAA {
+
+	public static void main(String[] args) {
+		int a=20;
+		int b=40;
+		int c=a+b;
+		System.out.println(c);
+
+	}
+
+}
