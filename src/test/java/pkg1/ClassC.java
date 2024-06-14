d304c8a368ca9a12072c4021081ad67676b36e19
vaibhavby07 <vaibhavby07@gmail.com>
2024-06-14 19:25:22 +0530

    newly added

---
+++ src/test/java/pkg1/ClassC.java
@@ -0,0 +1,16 @@
+package pkg1;
+
+public class ClassC
+{
+	public ClassC() 
+	{
+		System.out.println("constructor without parameter");
+		}
+	public static void main(String[] args)
+	{
+	   ClassC a=new ClassC();
+	}
+
+	}
+
+
