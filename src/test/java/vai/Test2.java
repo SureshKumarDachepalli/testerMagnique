108b3253cdb453ecd3e6aaefb3b76fd6c2e7f497
vaibhavby07 <vaibhavby07@gmail.com>
2024-06-16 17:45:50 +0530

    I am creating new class with new branch

---
+++ src/test/java/vai/Test2.java
@@ -0,0 +1,15 @@
+package vai;
+
+public class Test2 {
+
+
+		public static void page1()
+		{
+			System.out.println("welcome to underworld");
+		}
+		public static void main(String[] args) 
+		{
+           Test2.page1();
+	}
+
+}
