80868a2c6e5b659256ec74ccf483098dc8a37a14
vaibhavby07 <vaibhavby07@gmail.com>
2024-06-16 17:19:47 +0530

    i reviewed by me

--- src/test/java/vai/Test1.java
+++ src/test/java/vai/Test1.java
@@ -1,13 +1,13 @@
 package vai;
 
-public class Test1 
+public class Test1
 {
 	    public String param1;
 	    public String abri;
 	    
 //		public Test1()
 //		{
-//		   this.param1="my name is vaibhav b y";
+//		   this.param1="my name is vaibhav ";
 //		}
      	public Test1(String abri)
 		{
@@ -16,7 +16,7 @@ public class Test1
 		}
 		public static void main(String[] args) 
 		{
-			Test1 ob=new Test1("No1");
+			Test1 ob=new Test1("My name is vaibhav");
 			
 		//	System.out.println(ob.param1);
 			System.out.println(ob.abri);
